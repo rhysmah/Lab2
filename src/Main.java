20b57bc55892218e92b7ce1107355ced06b98d39
rhysmah <rhys.mahannah@gmail.com>
2023-05-26 11:58:41 -0700

    Lab 2 -- Update code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 // Drives the program.
@@ -14,11 +15,9 @@ public class Main {
         try {
             fileReader = new BufferedReader(new FileReader(filePath));
 
-            // Read through text; split tokens based on space character and add to array.
             String text = fileReader.readLine();
             String[] words = text.toLowerCase().split(" ");
 
-            // Run three different search techniques
             technique1(words);
             technique2(words);
             technique3(words);
@@ -34,14 +33,16 @@ public class Main {
     public static void technique1(String[] words) {
 
         double startTime = System.currentTimeMillis(); // Timer to count function runtime.
-        int[] anagramCounter = new int[words.length];  // Stores anagram counts for each word
+
+        int largestAnagramCount = 0;
+        int[] anagramCounter    = new int[words.length];  // Stores anagram counts for each word
 
         // Loop through all words in "words" array. Each word
         // "words[i]" will be the target word for that loop.
-        for (int i = 0; i < words.length; ++i) {
+        for (int wordsIndex = 0; wordsIndex < words.length; ++wordsIndex) {
 
             // Once target word identified, convert to char array.
-            char[] targetWord = words[i].toCharArray();
+            char[] targetWord = words[wordsIndex].toCharArray();
 
             // Compare all words to the target word. Each word
             // "words[j]" will be the comparison word.
@@ -50,7 +51,7 @@ public class Main {
                 // If "targetWord" and "comparisonWord" are same length AND
                 // "targetWord" and "comparisonWord" are NOT the same word,
                 // then they _may_ be an anagram.
-                if (words[i].length() == word.length() && !words[i].equals(word)) {
+                if (words[wordsIndex].length() == word.length() && !words[wordsIndex].equals(word)) {
 
                     // Break target word and comparison word into character arrays.
                     char[] comparisonWord = word.toCharArray();
@@ -60,9 +61,9 @@ public class Main {
                     // remove the first instance of that letter; if not, immediately skip to
                     // the next letter.
                     for (char targetWordLetter : targetWord) {
-                        for (int l = 0; l < comparisonWord.length; ++l) {
-                            if (targetWordLetter == comparisonWord[l]) {
-                                comparisonWord[l] = '\0';
+                        for (int letterIndex = 0; letterIndex < comparisonWord.length; ++letterIndex) {
+                            if (targetWordLetter == comparisonWord[letterIndex]) {
+                                comparisonWord[letterIndex] = '\0';
                                 break;
                             }
                         }
@@ -80,108 +81,116 @@ public class Main {
                     // If there are no letters left in the comparison word, the two words are
                     // anagrams, so we increase the count in the array.
                     if (letterNotFound) {
-                        ++anagramCounter[i];
+                        ++anagramCounter[wordsIndex];
                     }
                 }
             }
+            if (anagramCounter[wordsIndex] > largestAnagramCount) {
+                largestAnagramCount = anagramCounter[wordsIndex];
+            }
         }
         // Find the word with the most anagrams -- both the count and the word's index position.
-        int[] results        = anagramWithHighestCount(anagramCounter);
-        double endTime       = System.currentTimeMillis();
-        double executionTime = (endTime - startTime) / 1000.00; // Calculate time in seconds.
+        ArrayList<String> results     = countHighestAnagram(words, anagramCounter, largestAnagramCount);
+        double endTime                = System.currentTimeMillis();
+        double executionTime          = (endTime - startTime) / 1000.00; // Calculate time in seconds.
 
         System.out.println("*** SORTING TECHNIQUE 1 ***");
-        System.out.printf("Word with most anagrams: %s\n", words[results[1]]);
-        System.out.printf("How many anagrams does %s have: %d\n", words[results[1]], results[0]);
-        System.out.printf("Time to complete search (seconds): %.2f\n\n", executionTime);
+        System.out.printf("Word(s) with most anagrams (%d): \"%s\".\n", largestAnagramCount, results);
+        System.out.printf("Time to complete search (sec): %.2f\n\n", executionTime);
     }
 
-    // Anagram Checker -- Technique 2
+//  Anagram Checker -- Technique 2
     public static void technique2(String[] words) {
 
-        double startTime      = System.currentTimeMillis(); // Time to count function runtime.
-        int[]  anagramCounter = new int[words.length];      // Array to store anagram counts for each word.
+        double startTime = System.currentTimeMillis(); // Timer to count function runtime.
+
+        int   largestAnagramCount = 0;
+        int[] anagramCounter      = new int[words.length]; // Stores anagram counts for each word
 
         // Loop through all strings in "words" array.
         // Each word selected here becomes the "target" word.
-        for (int i = 0; i < words.length; ++i) {
+        for (int wordsIndex = 0; wordsIndex < words.length; ++wordsIndex) {
 
             // Once target word has been selected, convert to char array and sort.
-            char[] targetWord = words[i].toCharArray();
+            char[] targetWord = words[wordsIndex].toCharArray();
             Arrays.sort(targetWord);
 
             // Once a target word has been selected, loop through
             // remaining words; remaining words are "comparison" words.
-            for (int j = 0; j < words.length; ++j) {
+            for (String word : words) {
 
                 // Check that words are the same length AND that they're not the same word.
                 // If words _are_ the same length, they _may_ be anagrams.
-                if (words[i].length() == words[j].length() && !words[i].equals(words[j])) {
+                if (words[wordsIndex].length() == word.length() && !words[wordsIndex].equals(word)) {
 
                     // Convert strings to char arrays and sort.
-                    char[] comparisonWord = words[j].toCharArray();
+                    char[] comparisonWord = word.toCharArray();
                     Arrays.sort(comparisonWord);
 
                     // Loop through sorted arrays and check that they're equal.
-                    boolean areAnagrams = true;
+                    boolean isAnagram = true;
                     for (int k = 0; k < targetWord.length; ++k) {
                         if (targetWord[k] != comparisonWord[k]) {
-                            areAnagrams = false;
+                            isAnagram = false;
                             break;
                         }
                     }
                     // If the two sorted arrays are equal, they're anagrams; increase count.
-                    if (areAnagrams) {
-                        ++anagramCounter[i];
+                    if (isAnagram) {
+                        ++anagramCounter[wordsIndex];
                     }
+                }
+                if (anagramCounter[wordsIndex] > largestAnagramCount) {
+                    largestAnagramCount = anagramCounter[wordsIndex];
                 }
             }
         }
         // Find the word with the most anagrams -- both the count and the word's index position.
-        int[] results        = anagramWithHighestCount(anagramCounter);
-        double endTime       = System.currentTimeMillis();
-        double executionTime = (endTime - startTime) / 1000.00; // Calculate time in seconds.
+        ArrayList<String> results     = countHighestAnagram(words, anagramCounter, largestAnagramCount);
+        double endTime                = System.currentTimeMillis();
+        double executionTime          = (endTime - startTime) / 1000.00; // Calculate time in seconds.
 
         System.out.println("*** SORTING TECHNIQUE 2 ***");
-        System.out.printf("Word with most anagrams: %s\n", words[results[1]]);
-        System.out.printf("How many anagrams does %s have: %d\n", words[results[1]], results[0]);
-        System.out.printf("Time to complete search (seconds): %.2f\n\n", executionTime);
+        System.out.printf("Word(s) with most anagrams (%d): \"%s\".\n", largestAnagramCount, results);
+        System.out.printf("Time to complete search (sec): %.2f\n\n", executionTime);
     }
 
     // Anagram Checker -- Technique 3
     public static void technique3(String[] words) {
 
-        double startTime      = System.currentTimeMillis(); // Time to count function runtime.
-        int[]  anagramCounter = new int[words.length];      // Array to store anagram counts for each word.
+        double startTime = System.currentTimeMillis(); // Timer to count function runtime.
+
+        int   largestAnagramCount = 0;
+        int[] anagramCounter      = new int[words.length];  // Stores anagram counts for each word
 
         // Loop through all strings in "words" array.
         // Each word selected here becomes the "target" word.
-        for (int i = 0; i < words.length; ++i) {
+        for (int wordsIndex = 0; wordsIndex < words.length; ++wordsIndex) {
 
             // Once target word has been selected, convert to char array.
             int[] targetLetterCounter = new int[123];
-            char[] targetWord         = words[i].toCharArray();
+            char[] targetWord         = words[wordsIndex].toCharArray();
 
-            for (int targetIndex = 0; targetIndex < targetWord.length; ++targetIndex) {
-                ++targetLetterCounter[targetWord[targetIndex]];
+            for (char c : targetWord) {
+                ++targetLetterCounter[c];
             }
 
             // Once a target word has been selected, loop through
             // remaining words; remaining words are "comparison" words.
-            for (int j = 0; j < words.length; ++j) {
+            for (String word : words) {
 
                 // Check that words are the same length AND that they're not the same word.
                 // If words _are_ the same length, they _may_ be anagrams.
-                if (words[i].length() == words[j].length() && !words[i].equals(words[j])) {
+                if (words[wordsIndex].length() == word.length() && !words[wordsIndex].equals(word)) {
 
                     // Using larger size, so we can increment letter counts using letter ascii code.
                     int[] comparisonLetterCounter = new int[123];
 
                     // Convert strings to char arrays and sort.
-                    char[] comparisonWord = words[j].toCharArray();
+                    char[] comparisonWord = word.toCharArray();
 
-                    for (int comparisonIndex = 0; comparisonIndex < comparisonWord.length; ++comparisonIndex) {
-                        ++comparisonLetterCounter[comparisonWord[comparisonIndex]];
+                    for (char c : comparisonWord) {
+                        ++comparisonLetterCounter[c];
                     }
                     boolean isAnagram = true;
                     for (int letterCountCompare = 0; letterCountCompare < comparisonLetterCounter.length; ++letterCountCompare) {
@@ -191,35 +200,32 @@ public class Main {
                         }
                     }
                     if (isAnagram) {
-                        ++anagramCounter[i];
+                        ++anagramCounter[wordsIndex];
                     }
+                }
+                if (anagramCounter[wordsIndex] > largestAnagramCount) {
+                    largestAnagramCount = anagramCounter[wordsIndex];
                 }
             }
         }
         // Find the word with the most anagrams -- both the count and the word's index position.
-        int[] results        = anagramWithHighestCount(anagramCounter);
-        double endTime       = System.currentTimeMillis();
-        double executionTime = (endTime - startTime) / 1000.00; // Calculate time in seconds.
+        ArrayList<String> results     = countHighestAnagram(words, anagramCounter, largestAnagramCount);
+        double endTime                = System.currentTimeMillis();
+        double executionTime          = (endTime - startTime) / 1000.00; // Calculate time in seconds.
 
         System.out.println("*** SORTING TECHNIQUE 3 ***");
-        System.out.printf("Word with most anagrams: %s\n", words[results[1]]);
-        System.out.printf("How many anagrams does %s have: %d\n", words[results[1]], results[0]);
-        System.out.printf("Time to complete search (seconds): %.2f\n\n", executionTime);
+        System.out.printf("Word(s) with most anagrams (%d): \"%s\".\n", largestAnagramCount, results);
+        System.out.printf("Time to complete search (sec): %.2f\n\n", executionTime);
     }
 
-    public static int[] anagramWithHighestCount(int[] anagramCounter) {
-        int largestAnagramCount = 0;
-        int indexOfHighestCount = -1;
-        int[] results = new int[2];
+    public static ArrayList<String> countHighestAnagram(String[] listOfWords, int[]anagramCount, int highestCount) {
 
-        for (int i = 0; i < anagramCounter.length; ++i) {
-            if (anagramCounter[i] > largestAnagramCount) {
-                largestAnagramCount = anagramCounter[i];
-                indexOfHighestCount = i;
+        ArrayList<String> wordsWithHighestAnagramCount = new ArrayList<>();
+        for (int i = 0; i < listOfWords.length; ++i) {
+            if (anagramCount[i] == highestCount) {
+                wordsWithHighestAnagramCount.add(listOfWords[i]);
             }
         }
-        results[0] = largestAnagramCount;
-        results[1] = indexOfHighestCount;
-        return results;
+        return wordsWithHighestAnagramCount;
     }
 }
